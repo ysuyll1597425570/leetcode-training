87dcd3eb60e596c0123d52d8af8493ae6b6a6e1e
yangforeverliang <yyllyy1597425570@gmail.com>
2021-04-14 16:20:21 +0800

    added a lcm function

--- math/gcd.py
+++ math/gcd.py
@@ -1,8 +1,13 @@
-#求解最大公约数
+#求解最大公约数和最小公倍数
 class solution:
     def gcd(self, a, b):
         if b == 0:
             return a
         else:
             self.gcd(b, a%b)
+            
+    
+    
+    def lcm(self, a, b):
+        return a * b / self.gcd(a,b)
     
